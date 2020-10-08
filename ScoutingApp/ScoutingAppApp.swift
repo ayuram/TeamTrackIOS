1459ef84fdf4d1014a305e2e61d480d027066a30
ayuram <rama5398@lgsstudent.org>
2020-10-08 00:03:52 -0700

    Initial Commit

---
+++ ScoutingApp/ScoutingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutingAppApp.swift
+//  ScoutingApp
+//
+//  Created by Ayush Raman on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct ScoutingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
